e463958d6638295c298362a326c18eed17db21c9
Dave Wiper <dwiper@idreaminteractive.com>
2025-01-13 03:34:24 +0000

    chore: things

--- kv/lib/kv.ex
+++ kv/lib/kv.ex
@@ -1,21 +1,25 @@
-defmodule BirdCount do
-  def today(list) do
-    # Please implement the today/1 function
+defmodule HighScore do
+  def new() do
+    # Please implement the new/0 function
   end
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
+  def add_player(scores, name, score) do
+    # Please implement the add_player/3 function
   end
 
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
+  def remove_player(scores, name) do
+    # Please implement the remove_player/2 function
   end
 
-  def total(list) do
-    # Please implement the total/1 function
+  def reset_score(scores, name) do
+    # Please implement the reset_score/2 function
   end
 
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
+  def update_score(scores, name, score) do
+    # Please implement the update_score/3 function
+  end
+
+  def get_players(scores) do
+    # Please implement the get_players/1 function
   end
 end
