8fe9434505831fe12bb0eb1eb6fa7d03644e07f2
Dave Wiper <dwiper@idreaminteractive.com>
2025-01-14 11:27:30 +0000

    sabing

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -8,6 +8,7 @@ defmodule HelloWeb.Router do
     plug(:put_root_layout, html: {HelloWeb.Layouts, :root})
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
+    plug(HelloWeb.Plugs.Locale, "en")
   end
 
   pipeline :api do
@@ -19,6 +20,7 @@ defmodule HelloWeb.Router do
 
     get("/", PageController, :home)
     get("/hello", HelloController, :index)
+    get("/hello/:messenger", HelloController, :show)
   end
 
   # Other scopes may use custom stacks.
